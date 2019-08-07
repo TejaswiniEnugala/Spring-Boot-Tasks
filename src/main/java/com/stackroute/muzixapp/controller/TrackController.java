145fd4440e43f979db6734aa08489f0063e5477a
TejaswiniEnugala <tejaswinienugala.in@gmail.com>
2019-08-07 10:04:37 +0530

    task2 with changes completed

---
+++ src/main/java/com/stackroute/muzixapp/controller/TrackController.java
@@ -0,0 +1,57 @@
+package com.stackroute.muzixapp.controller;
+import com.stackroute.muzixapp.service.TrackDAO;
+import com.stackroute.muzixapp.domain.Track;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "api/v1")
+public class TrackController {
+
+	@Autowired
+	private TrackDAO trackDAO;
+
+	public TrackController(TrackDAO trackDAO)
+	{
+		this.trackDAO=trackDAO;
+	}
+//save the track
+	@PostMapping("track")
+	public ResponseEntity<?> saveTrack(@RequestBody Track track)
+	{
+		ResponseEntity responseEntity;
+		try {
+			trackDAO.saveTrack(track);
+			responseEntity = new ResponseEntity<String>("successfully created", HttpStatus.CREATED);
+		} finally {
+
+		}
+
+		return responseEntity;
+
+	}
+	//delete the track
+	@DeleteMapping("track")
+	public ResponseEntity<?> deleteTrack(@RequestBody Track track)
+	{
+		ResponseEntity responseEntity;
+		try {
+
+			trackDAO.deleteTrack(track.getId());
+			responseEntity = new ResponseEntity<String>("deleted successfully", HttpStatus.CREATED);
+		} finally {
+
+		}
+		return responseEntity;
+	}
+//get all tracks
+	@GetMapping("track")
+	public ResponseEntity<?> getAllTracks(){
+		return new ResponseEntity<List<Track>>(trackDAO.getAllTracks(),HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/stackroute/muzixapp/service/TrackDAOImpl.java
@@ -0,0 +1,59 @@
+package com.stackroute.muzixapp.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.stackroute.muzixapp.repository.TrackRepository;
+import org.hibernate.SessionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.stackroute.muzixapp.domain.Track;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrackDAOImpl implements TrackDAO {
+private TrackRepository trackRepository;
+	@Autowired
+	public TrackDAOImpl(TrackRepository trackRepository) {
+		this.trackRepository=trackRepository;
+	}
+	//implement all the methods
+	@Override
+	//override save track method
+	public Track saveTrack(Track track){
+
+		Track savedTrack=trackRepository.save(track);
+
+		return savedTrack;
+	}
+	@Override
+	//override delete track method
+	public List<Track> deleteTrack(int id) {
+
+		trackRepository.deleteById(id);
+return trackRepository.findAll();
+
+	}
+
+//to get all tracks
+	@Override
+	public List<Track> getAllTracks() {
+		return trackRepository.findAll();
+	}
+
+	@Override
+	public Track getTrackById(int id) {
+		return trackRepository.save(getTrackById(id));
+
+	}
+//to update tracks
+	@Override
+	public Track UpdateTrack(Track track) {
+		Track tracks=trackRepository.save(track);
+		return tracks;
+	}
+
+}
